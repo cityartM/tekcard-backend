4edd2a663a5a8cf57fc8615a72d15b5b0c473314
Mouadh <bekhoche.mouadh@gmail.com>
2023-10-14 10:22:30 +0100

    compiled assets for production

--- resources/frontend/js/Layouts/AuthenticatedLayout.tsx
+++ resources/frontend/js/Layouts/AuthenticatedLayout.tsx
@@ -1,10 +1,10 @@
 import { useState, PropsWithChildren, ReactNode } from 'react';
-import ApplicationLogo from '@/Components/ApplicationLogo';
-import Dropdown from '@/Components/Dropdown';
-import NavLink from '@/Components/NavLink';
-import ResponsiveNavLink from '@/Components/ResponsiveNavLink';
+import ApplicationLogo from '../Components/ApplicationLogo';
+import Dropdown from '../Components/Dropdown';
+import NavLink from '../Components/NavLink';
+import ResponsiveNavLink from '../Components/ResponsiveNavLink';
 import { Link } from '@inertiajs/react';
-import { User } from '@/types';
+import { User } from '../types';
 
 export default function Authenticated({ user, header, children }: PropsWithChildren<{ user: User, header?: ReactNode }>) {
     const [showingNavigationDropdown, setShowingNavigationDropdown] = useState(false);
