eb17f398a3ac1e13bac6260dd6ee717db3e26336
BoulmaizMohamed <samadante1@gmail.com>
2023-10-19 15:52:43 +0100

    apply ajax tables on feedback contat and subscripe

--- app/Support/Plugins/Plans.php
+++ app/Support/Plugins/Plans.php
@@ -10,7 +10,7 @@ class Plans extends Plugin
     public function sidebar()
     {
         return Item::create(__('app.plans'))
-            ->route('')
+            ->route('plans.index')
             ->icon('<span class="svg-icon svg-icon-2"><i class="fa fa-cubes mx-1"></i></span>')
             ->active("plans*")
             ->permissions('plans.manage');
