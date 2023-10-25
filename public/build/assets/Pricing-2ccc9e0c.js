84e5f985dccc9277f847c124934f8b8f4753b3b9
Mouadh <bekhoche.mouadh@gmail.com>
2023-10-25 09:59:42 +0100

    built assets

--- public/build/assets/Pricing-2ccc9e0c.js
+++
@@ -1,1 +0,0 @@
-import{j as i,a as t}from"./front-5b691397.js";import{L as r}from"./LandingLayout-49510915.js";import"./bootstrap-dd2a6c2f.js";/* empty css            */function n({}){return i.jsxs(r,{children:[i.jsx(t,{title:"Welcome"}),i.jsx("div",{className:"text-3xl",children:"Pricing"})]})}export{n as default};
