f045b65a0ca015b30e89d1016390ace37b34041a
BoulmaizMohamed <samadante1@gmail.com>
2023-10-26 11:50:23 +0100

    blog

--- Modules/Blog/Routes/web.php
+++ Modules/Blog/Routes/web.php
@@ -18,3 +18,6 @@ Route::prefix(LaravelLocalization::setLocale().'/')->group(function(){
     Route::resource('blogs', \Modules\Blog\Http\Controllers\BlogController::class)->middleware('permission:blogs.manage');
 
 });
+
+Route::get('/blogs/{id}', 'BlogController@show')->name('blogs.show');
+Route::get('/blogsmain', 'BlogController@all')->name('blogs.all');
