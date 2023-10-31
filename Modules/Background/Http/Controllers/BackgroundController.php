6c502d6cb03ae9af6a0bc9a625226d82283655c2
doudouinfo <daoudbelmerabet@gmail.com>
2023-10-31 15:25:39 +0100

    update: fix bug in background

--- Modules/Background/Http/Controllers/BackgroundController.php
+++ Modules/Background/Http/Controllers/BackgroundController.php
@@ -28,8 +28,6 @@ class BackgroundController extends Controller
      */
     public function index(Request $request)
     {
-        $background = Background::where('type', 'Share')->first();
-        dd($background->getFirstMediaUrl('background'));
         if ($request->wantsJson()) {
             return $this->background->getDatatables()->datatables($request);
         }
--- Modules/Background/Models/Background.php
+++ Modules/Background/Models/Background.php
@@ -3,11 +3,9 @@
 namespace Modules\Background\Models;
 
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\Factories\HasFactory; 
-
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Spatie\MediaLibrary\HasMedia;
 use Spatie\MediaLibrary\InteractsWithMedia;
-
 use App\Support\Enum\BackgroundType;
 
 class Background extends Model implements HasMedia
@@ -16,16 +14,13 @@ class Background extends Model implements HasMedia
 
     protected $fillable = ['type'];
 
-     
+
     public static function backgroundTypes()
     {
         return BackgroundType::lists();
     }
 
 
-
-
-    
     protected static function newFactory()
     {
         return \Modules\Background\Database\factories\BackgroundFactory::new();
