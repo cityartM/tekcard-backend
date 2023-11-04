f9f4a6c892f55401cfccf6f2c3f56298c73504a6
Mouadh <bekhoche.mouadh@gmail.com>
2023-11-04 15:20:35 +0100

    added blog Typescript types

--- resources/js/Pages/Blog.tsx
+++ resources/js/Pages/Blog.tsx
@@ -19,60 +19,6 @@ export default function Blog({}: PropsWithChildren) {
   );
 }
 
-/*const posts: Post[] = [
-  {
-    id: 1,
-    title: 'Boost your conversion rate',
-    href: '#',
-    description:
-      'Illo sint voluptas. Error voluptates culpa eligendi. Hic vel totam vitae illo. Non aliquid explicabo necessitatibus unde. Sed exercitationem placeat consectetur nulla deserunt vel. Iusto corrupti dicta.',
-    imageUrl:
-      'https://images.unsplash.com/photo-1496128858413-b36217c2ce36?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=3603&q=80',
-    date: 'Mar 16, 2020',
-    datetime: '2020-03-16',
-    category: { title: 'Marketing', href: '#' },
-    author: {
-      name: 'Michael Foster',
-      role: 'Co-Founder / CTO',
-      href: '#',
-      imageUrl: 'https://images.unsplash.com/photo-1519244703995-f4e0f30006d5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
-    },
-  },
-  {
-    id: 2,
-    title: 'Boost your conversion rate',
-    href: '#',
-    description: 'Illo sint voluptas. Error voluptates culpa eligendi. Hic vel totam vitae illo. Non aliquid explicabo necessitatibus unde. Sed exercitationem placeat consectetur nulla deserunt vel. Iusto corrupti dicta.',
-    imageUrl: 'https://images.unsplash.com/photo-1547586696-ea22b4d4235d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=3270&q=80',
-    date: 'Mar 16, 2020',
-    datetime: '2020-03-16',
-    category: { title: 'Marketing', href: '#' },
-    author: {
-      name: 'Michael Foster',
-      role: 'Co-Founder / CTO',
-      href: '#',
-      imageUrl: 'https://images.unsplash.com/photo-1519244703995-f4e0f30006d5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
-    },
-  },
-  {
-    id: 3,
-    title: 'Boost your conversion rate',
-    href: '#',
-    description: 'Illo sint voluptas. Error voluptates culpa eligendi. Hic vel totam vitae illo. Non aliquid explicabo necessitatibus unde. Sed exercitationem placeat consectetur nulla deserunt vel. Iusto corrupti dicta.',
-    imageUrl: 'https://images.unsplash.com/photo-1492724441997-5dc865305da7?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=3270&q=80',
-    date: 'Mar 16, 2020',
-    datetime: '2020-03-16',
-    category: { title: 'Marketing', href: '#' },
-    author: {
-      name: 'Michael Foster',
-      role: 'Co-Founder / CTO',
-      href: '#',
-      imageUrl: 'https://images.unsplash.com/photo-1519244703995-f4e0f30006d5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
-    },
-  },
-  // More posts...
-]*/
-
 const Section = ({children, className}: PropsWithChildren & {className?: string}) => {
   return (
     <div className={`min-h-screen h-full flex flex-col ${className}`}>
@@ -120,18 +66,6 @@ const BlogPostCard = ({post}: PropsWithChildren & {className?: string, post: Pos
           </h3>
           <p className="mt-5 line-clamp-3 text-sm leading-6 text-gray-600">{post.description}</p>
         </div>
-        {/*<div className="relative mt-8 flex items-center gap-x-4">
-                        <img src={post.author.imageUrl} alt="" className="h-10 w-10 rounded-full bg-gray-100" />
-                        <div className="text-sm leading-6">
-                          <p className="font-semibold text-gray-900">
-                            <a href={post.author.href}>
-                              <span className="absolute inset-0" />
-                              {post.author.name}
-                            </a>
-                          </p>
-                          <p className="text-gray-600">{post.author.role}</p>
-                        </div>
-                      </div>*/}
       </div>
     </article>
   );
--- resources/js/Pages/BlogSingle.tsx
+++ resources/js/Pages/BlogSingle.tsx
@@ -4,24 +4,7 @@ import LandingLayout from "../Layouts/LandingLayout";
 import TextInput from "@/Components/Input";
 import ApplicationLogo from "@/Components/ApplicationLogo";
 import {ErrorBag, Errors, PageProps} from "@/types";
-
-/*const post: Post = {
-  id: 1,
-  title: 'Boost your conversion rate',
-  href: '#',
-  description: 'Illo sint voluptas. Error voluptates culpa eligendi. Hic vel totam vitae illo. Non aliquid explicabo necessitatibus unde. Sed exercitationem placeat consectetur nulla deserunt vel. Iusto corrupti dicta.',
-  content: '<p>Illo sint voluptas. Error voluptates culpa eligendi. Hic vel totam vitae illo. Non aliquid explicabo necessitatibus unde. Sed exercitationem placeat consectetur nulla deserunt vel. Iusto corrupti dicta.</p>\r\n<p style="text-align: left;">&nbsp;<\/p>\r\n<p style="text-align: left;"><img src="https://images.unsplash.com/photo-1682687982470-8f1b0e79151a?auto=format&amp;fit=crop&amp;q=60&amp;w=800&amp;ixlib=rb-4.0.3&amp;ixid=M3wxMjA3fDF8MHxlZGl0b3JpYWwtZmVlZHw2fHx8ZW58MHx8fHx8" alt="fsdfdsfs" width="772" height="162" /></p>',
-  thumbnail: 'https://images.unsplash.com/photo-1496128858413-b36217c2ce36?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=3603&q=80',
-  date: 'Mar 16, 2020',
-  datetime: '2020-03-16',
-  category: { title: 'Marketing', href: '#' },
-  author: {
-    name: 'Michael Foster',
-    role: 'Co-Founder / CTO',
-    href: '#',
-    imageUrl: 'https://images.unsplash.com/photo-1519244703995-f4e0f30006d5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
-  },
-}*/
+import {Post} from "@/types/blog";
 
 export default function BlogSingle({}: PropsWithChildren) {
 
@@ -51,7 +34,7 @@ export default function BlogSingle({}: PropsWithChildren) {
                   type={'text'}
                   placeholder={'Search'}
                   value={''}
-                  onChange={(event) => {}}
+                  onChange={(event) => (event.target.value)}
                 />
                 {/*{errors && <InputError message={errors}/>}*/}
               </div>
@@ -65,63 +48,6 @@ export default function BlogSingle({}: PropsWithChildren) {
   );
 }
 
-const posts: Post[] = [
-  {
-    id: 1,
-    title: 'Boost your conversion rate',
-    href: '#',
-    description:
-      'Illo sint voluptas. Error voluptates culpa eligendi. Hic vel totam vitae illo. Non aliquid explicabo necessitatibus unde. Sed exercitationem placeat consectetur nulla deserunt vel. Iusto corrupti dicta.',
-    content: '<p style="text-align: left;">Illo sint voluptas. Error voluptates culpa eligendi. Hic vel totam vitae illo. Non aliquid explicabo necessitatibus unde. Sed exercitationem placeat consectetur nulla deserunt vel. Iusto corrupti dicta.</p>\r\n<p style="text-align: left;">&nbsp;<\/p>\r\n<p style="text-align: left;"><img src="https://images.unsplash.com/photo-1682687982470-8f1b0e79151a?auto=format&amp;fit=crop&amp;q=60&amp;w=800&amp;ixlib=rb-4.0.3&amp;ixid=M3wxMjA3fDF8MHxlZGl0b3JpYWwtZmVlZHw2fHx8ZW58MHx8fHx8" alt="fsdfdsfs" width="772" height="162" /></p>',
-    thumbnail:
-      'https://images.unsplash.com/photo-1496128858413-b36217c2ce36?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=3603&q=80',
-    date: 'Mar 16, 2020',
-    datetime: '2020-03-16',
-    category: { title: 'Marketing', href: '#' },
-    author: {
-      name: 'Michael Foster',
-      role: 'Co-Founder / CTO',
-      href: '#',
-      imageUrl: 'https://images.unsplash.com/photo-1519244703995-f4e0f30006d5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
-    },
-  },
-  {
-    id: 2,
-    title: 'Boost your conversion rate',
-    href: '#',
-    description: 'Illo sint voluptas. Error voluptates culpa eligendi. Hic vel totam vitae illo. Non aliquid explicabo necessitatibus unde. Sed exercitationem placeat consectetur nulla deserunt vel. Iusto corrupti dicta.',
-    content: "",
-    thumbnail: 'https://images.unsplash.com/photo-1547586696-ea22b4d4235d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=3270&q=80',
-    date: 'Mar 16, 2020',
-    datetime: '2020-03-16',
-    category: { title: 'Marketing', href: '#' },
-    author: {
-      name: 'Michael Foster',
-      role: 'Co-Founder / CTO',
-      href: '#',
-      imageUrl: 'https://images.unsplash.com/photo-1519244703995-f4e0f30006d5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
-    },
-  },
-  {
-    id: 3,
-    title: 'Boost your conversion rate',
-    href: '#',
-    description: 'Illo sint voluptas. Error voluptates culpa eligendi. Hic vel totam vitae illo. Non aliquid explicabo necessitatibus unde. Sed exercitationem placeat consectetur nulla deserunt vel. Iusto corrupti dicta.',
-    content: "",
-    thumbnail: 'https://images.unsplash.com/photo-1492724441997-5dc865305da7?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=3270&q=80',
-    date: 'Mar 16, 2020',
-    datetime: '2020-03-16',
-    category: { title: 'Marketing', href: '#' },
-    author: {
-      name: 'Michael Foster',
-      role: 'Co-Founder / CTO',
-      href: '#',
-      imageUrl: 'https://images.unsplash.com/photo-1519244703995-f4e0f30006d5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
-    },
-  },
-  // More posts...
-]
-
 const Section = ({children, className}: PropsWithChildren & {className?: string}) => {
   return (
     <div className={`min-h-screen h-full flex flex-col ${className}`}>
@@ -131,10 +57,6 @@ const Section = ({children, className}: PropsWithChildren & {className?: string}
     </div>
   );
 }
-
-type category = {title: string, href: string}
-type author = {name: string, role: string, href: string, imageUrl: string}
-type Post = {id: number, title: string, href: string, description: string, content: string | TrustedHTML, thumbnail: string, date: string, datetime: string, category: category, author: author}
 
 const PostList = ({posts}: PropsWithChildren & {className?: string, posts: Post[]}) => {
   return (
