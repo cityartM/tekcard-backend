d973d58eff44ef9e5f72c893b00a3be47baf7e6d
doudouinfo <daoudbelmerabet@gmail.com>
2023-11-05 07:15:12 +0100

    update: seeder

--- Modules/GlobalSetting/Database/Seeders/GlobalSettingDatabaseSeeder.php
+++ Modules/GlobalSetting/Database/Seeders/GlobalSettingDatabaseSeeder.php
@@ -62,7 +62,7 @@ class GlobalSettingDatabaseSeeder extends Seeder
         {
             foreach($socialMediaPlatform as $k => $value)
             {
-                $settingContact = SettingContact::create([
+                    $a = \DB::table('setting_contacts')->insert([
                         [
                             'display_name' => $value,
                             'category' => $key,
@@ -72,8 +72,8 @@ class GlobalSettingDatabaseSeeder extends Seeder
                         ],
 
                     ]);
-                //$settingContact = SettingContact::find($i);
-                $settingContact->addMedia(public_path('assets/socialMedia/'.$value.'.svg'))->toMediaCollection(ContactType::ICONCONTACT);
+                $settingContact = SettingContact::find($i);
+                $settingContact->addMedia(public_path('socialMedia/'.$value.'.svg'))->toMediaCollection(ContactType::ICONCONTACT);
                 $i++;
             }
         }
