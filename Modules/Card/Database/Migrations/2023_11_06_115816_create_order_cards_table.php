48b7ef7e32d314c95c9a2a2ae84634cc550053fd
BoulmaizMohamed <samadante1@gmail.com>
2023-11-07 07:47:06 +0000

    add status into database table of cards order

--- Modules/Card/Database/Migrations/2023_11_06_115816_create_order_cards_table.php
+++ Modules/Card/Database/Migrations/2023_11_06_115816_create_order_cards_table.php
@@ -3,6 +3,8 @@
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
+use Modules\Card\Support\OrderStatus;
+
 
 return new class extends Migration
 {
@@ -19,8 +21,8 @@ return new class extends Migration
             $table->integer('quantity');
             $table->string('color');
             $table->unsignedBigInteger("company_id")->nullable();
-
-
+           // $table->enum("status", OrderStatus::lists())->default('Pending'); didnt work it save as (app.orderStatus . pending or delivring )in database 
+            $table->enum('status',array_keys(OrderStatus::lists()))->default('Pending');
             $table->foreign('company_id')->references('id')->on('companies')->onDelete('set null');
             $table->foreign('card_id')->references('id')->on('cards')->onDelete('cascade');
 
--- Modules/Card/Models/CardOrder.php
+++ Modules/Card/Models/CardOrder.php
@@ -18,6 +18,7 @@ class CardOrder extends Model
         'quantity',
         'color',
         'company_id',
+        'status'
     ];
 
     public function company()
