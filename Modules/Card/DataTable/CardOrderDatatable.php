97ab52d3ca6397d34cd51e3c248ca37d438e6093
BoulmaizMohamed <samadante1@gmail.com>
2023-11-07 10:27:33 +0000

    order card ajout

--- Modules/Card/DataTable/CardOrderDatatable.php
+++ Modules/Card/DataTable/CardOrderDatatable.php
@@ -19,6 +19,7 @@ class CardOrderDatatable
             "quantity",
             "color",
             "company",
+            "status_order",
             "created_at",
         ];
     }
@@ -29,28 +30,34 @@ class CardOrderDatatable
             return datatables($this->query($request))
                 ->addColumn("action", function (CardOrder $card) {
                     return (new DataTableActions())
-                        ->edit(route("cardOrders.edit", $card->id))
                         ->delete(route("cardOrders.destroy", $card->id))
                         ->make();
                 })
                 
                 ->addColumn("card", function (CardOrder $card) {
-                    return $card->card->full_name ;
+                    return $card->card_id;
                 })
                 ->addColumn("quantity", function (CardOrder $card) {
                     return $card->quantity ;
                 })
                 ->addColumn("color", function (CardOrder $card) {
-                    return $card->color ;
+                    return (new DataTableActions())->color($card->color)  ;
                 })
                 ->addColumn("company", function (CardOrder $card) {
-                    return $card->company->full_name ;
+                    if ($card->company) {
+                        return $card->company->name;
+                    } else {
+                        return 'N/A'; // Replace with an appropriate message for when the company is not available.
+                    }
+                })
+                ->addColumn("status_order", function (CardOrder $card) {
+                    return $card->status;
                 })
                 ->addColumn("created_at", function (Blog $blog) {
                     return $blog->created_at->format('Y-m-d');
                 })
 
-                ->rawColumns(['action',"card","quantity","color","company","created_at",])
+                ->rawColumns(['action',"card","quantity","color","company","status_order","created_at",])
 
                 ->make(true);
         } catch (Exception $e) {
