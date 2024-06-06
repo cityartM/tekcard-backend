1c770d2a4b1b14eed4166bbcec65c667ed8c120e
doudouinfo <daoudbelmerabet@gmail.com>
2024-06-06 09:17:06 +0100

    update:registration with free plan

--- app/Http/Controllers/Api/Auth/RegistrationController.php
+++ app/Http/Controllers/Api/Auth/RegistrationController.php
@@ -15,6 +15,9 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use App\Repositories\Role\RoleRepository;
 use Illuminate\Support\Facades\Http;
+use Modules\Plan\Models\Plan;
+use Modules\Plan\Repositories\UserPlanRepository;
+use Modules\Plan\Support\Enum\PlanDuration;
 
 class RegistrationController extends ApiController
 {
@@ -28,17 +31,19 @@ class RegistrationController extends ApiController
      */
     private $roles;
     private $avatarManager;
+    private $userPlans;
 
     /**
      * Create a new authentication controller instance.
      * @param UserRepository $users
      * @param RoleRepository $roles
      */
-    public function __construct(UserRepository $users, RoleRepository $roles,UserAvatarManager $avatarManager)
+    public function __construct(UserRepository $users, RoleRepository $roles,UserAvatarManager $avatarManager,UserPlanRepository $userPlans)
     {
         $this->users = $users;
         $this->roles = $roles;
         $this->avatarManager = $avatarManager;
+        $this->userPlans = $userPlans;
     }
 
     /**
@@ -77,6 +82,40 @@ class RegistrationController extends ApiController
                     'email_verified_at' => Carbon::now()->format('Y-m-d'),
                 ])
             );
+
+        $plan = Plan::where([
+            'type' =>'Client' ,
+            'price' => 0
+        ])->first();
+
+        $dataPlan = [
+            'display_name'=> $plan->getTranslations('display_name'),
+            'type' => $plan->type,
+            'duration' => $plan->duration,
+            'purchase_date' => now()->format('Y-m-d'),
+            'expired_date' => $plan->duration == PlanDuration::YEARLY ? now()->addYear()->format('Y-m-d') : now()->addMonth()->format('Y-m-d'),
+            'price' => $plan->price,
+            'nbr_user' => $plan->nbr_user,
+            'nbr_card_user' => $plan->nbr_card_user,
+            'has_dashboard' => $plan->has_dashboard,
+            'has_video' => $plan->has_video ?? 0,
+            'has_pdf' => $plan->has_pdf ?? 0,
+            'has_multiple_image' => $plan->has_multiple_image ?? 0,
+            'has_water_mark' => $plan->has_water_mark ?? 0,
+            'has_share_offline' => $plan->has_share_offline ?? 0,
+            'share_with_image' => $plan->share_with_image ?? 0,
+            'has_scan_ia' => $plan->has_scan_ia ?? 0,
+            'has_group_contact' => $plan->has_group_contact ?? 0,
+            'has_scan_location' => $plan->has_scan_location ?? 0,
+            'has_note_contact' => $plan->has_note_contact ?? 0,
+            'has_statistic' => $plan->has_statistic ?? 0,
+            'features' => $plan->features,
+            'user_id' => $user->id,
+            'plan_id' => $plan->id,
+        ];
+
+
+        $this->userPlans->create($dataPlan);
 
             Auth::setUser($user);
 
@@ -146,7 +185,7 @@ class RegistrationController extends ApiController
         $zapierWebhookUrl = 'https://hooks.zapier.com/hooks/catch/17045830/30pesh5/';
         Http::post($zapierWebhookUrl, [
             'user' => $user,
-            
+
         ]);
     }
 }
