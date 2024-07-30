71295d1c78d5b9e0a5db0fb6ae823699f7663843
doudouinfo <daoudbelmerabet@gmail.com>
2024-07-30 11:36:51 +0100

    update:icon

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,8 +8,8 @@
     <meta name="description" content="TekCard" />
     <meta name="keywords" content="TekCard" />
     <meta name="viewport" content="width=device-width, initial-scale=1" />
-    <link rel="icon" type="image/png" href="{{ url('assets/media/logos/tekCard.png') }}" sizes="32x32" />
-    <link rel="icon" type="image/png" href="{{ url('assets/media/logos/tekCard.png') }}" sizes="16x16" />
+    <link rel="icon" type="image/png" href="{{ url('assets/media/logos/icon.png') }}" sizes="32x32" />
+    <link rel="icon" type="image/png" href="{{ url('assets/media/logos/icon.png') }}" sizes="16x16" />
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
 
